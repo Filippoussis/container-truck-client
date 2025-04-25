e9b36717442d79396a74dca7b034631b2c74b76a
Andrey Filippov <androsphilippos@gmail.com>
2025-04-25 09:12:01 +0500

    fix: setting axios lib

---
+++ src/libs/axios/axios.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosInstance } from 'axios';
+import { router } from '@/router';
+import { HTTP_STATUS_CODE_401 } from './constants';
+
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL: 'http://localhost:3001',
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === HTTP_STATUS_CODE_401) {
+      router.navigate('/login');
+      return new Promise(() => {});
+    }
+    return Promise.reject(error);
+  },
+);
