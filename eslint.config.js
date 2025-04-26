970dc69286fd84731170246cc0d5bbfb7bb42191
Andrey Filippov <androsphilippos@gmail.com>
2025-04-26 12:36:51 +0500

    fix: fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -21,10 +21,6 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
       '@tanstack/query/exhaustive-deps': 'error',
     },
   },
