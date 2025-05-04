e64ef4d157e948f2d23f414bbaa598af83187021
Andrey Filippov <androsphilippos@gmail.com>
2025-05-04 19:22:49 +0500

    feat: setting like PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        lang: 'ru',
+        name: 'Container Truck',
+        short_name: 'Container Truck',
+        theme_color: '#448aff',
+        background_color: '#448aff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: '/truck-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/truckM-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
