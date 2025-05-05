b7c24cdbad77a575339a224c374784479c454eb4
Andrey Filippov <androsphilippos@gmail.com>
2025-05-05 15:58:59 +0500

    fix: add 3001 port to axiosInstance

--- src/libs/axios/axios.ts
+++ src/libs/axios/axios.ts
@@ -3,7 +3,7 @@ import { router } from '@/router';
 import { HTTP_STATUS_CODE_401 } from './constants';
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: `http://${window.location.host}`,
+  baseURL: `http://${window.location.host}:3001`,
   withCredentials: true,
 });
 
